e34a7121895f6be7a1f9a79e8e168a40e7022797
Lasya Kambhampati <84462162+lasya02@users.noreply.github.com>
2023-05-09 14:56:07 -0400

    idrk what i did here

--- server.js
+++ server.js
@@ -11,25 +11,29 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/app', (req, res) => {
-	res.status(200).send("200 OK");
+app.get('/app', (_req, res) => {
+	res.status(200).send("200 OK")
 })
 
+
 app.get('/app/rps', (req, res) => {
-	res.status(200).send(rps());
+	res.status(200).send(JSON.stringify(rps(req.body.shot)))
 })
 
 app.get('/app/rpsls', (req, res) => {
-	res.status(200).send(rpsls());
+	res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
 })
+
 
 app.get('/app/rps/play', (req, res) => {
-	res.status(200).send(rps(req.query.shot));
+	res.status(200).send(JSON.stringify(rps(req.query.shot)))
 })
 
+
 app.get('/app/rpsls/play', (req, res) => {
-	res.status(200).send(rpsls(req.query.shot));
+	res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
 })
+
 
 app.post('/app/rps/play', (req, res) => {
 	res.status(200).send(JSON.stringify(rps(req.body.shot)))
@@ -49,7 +53,6 @@ app.get('/app/rps/play/:shot', (req, res) => {
 app.get('/app/rpsls/play/:shot', (req, res) => {
 	res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
 })
-
 
 app.get('*', (req, res) => {
 	res.status(404).send('404 NOT FOUND');
